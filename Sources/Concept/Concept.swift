d720886aafacae091a2b47747419466d200dc7c1
Mark Morrill <mark@automatontec.ca>
2022-05-17 20:40:59 -0700

    Update Concept.swift

--- Sources/Concept/Concept.swift
+++ Sources/Concept/Concept.swift
@@ -1,6 +1,9 @@
-public struct Concept {
+import Foundation
+
+public class Concept : NSObject {
     public private(set) var text = "Hello, World!"
 
-    public init() {
+    public override init() {
+        super.init()
     }
 }
